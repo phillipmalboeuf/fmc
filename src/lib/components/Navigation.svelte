3b1bfc53aca072d30ad5493482f8cdf5cca942ad
Phillip Malboeuf <phil@phils.computer>
2022-01-10 10:30:58 -0500

    navigation component

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+  import type { Entry } from 'contentful'
+  import { slide } from 'svelte/transition'
+
+  import Link from './Link.svelte'
+  import type { Lien } from './Link.svelte'
+  import { getContext, onMount, setContext } from 'svelte'
+  import Logo from './Logo.svelte'
+
+
+  export let contentHeight: number
+  export let path: string
+  export let index: boolean
+
+  const { main } = getContext<{ main: Entry<{ links: Entry<Lien>[] }> }>('navigation')
+
+  let scrollY: number
+  let innerHeight: number
+  let navHeight: number
+</script>
+
+<svelte:window bind:scrollY bind:innerHeight />
+
+<nav>
+  <div bind:offsetHeight={navHeight}>
+    <a href="/">
+      <Logo />
+    </a>
+
+    {#if main}
+    {#each main.fields.links as link}
+    <Link {link} on:click={e => {
+      if (index && !link.fields.path.startsWith('/#')) {
+        e.preventDefault()
+        history.pushState({}, null, link.fields.path)
+        window.scrollTo({ top: document.getElementById(link.fields.path.replace('/', '')).offsetTop, behavior: 'smooth' })
+        path = link.fields.path
+      }
+    }} /><br>
+    {/each}
+    {:else}
+    <!-- {#if page.fields.content}
+    {#each page.fields.content.filter(c => c.fields.id) as entry}
+    <a href="#{entry.fields.id}">{entry.fields.title}</a><br>
+    {/each}
+    {/if} -->
+    {/if}
+  </div>
+
+  {#if scrollY !== undefined}
+  <!-- <progress value={scrollY + 1} max={Math.max(offsetHeight - innerHeight, 1)} /> -->
+  <progress style="width: calc(95vh - {navHeight}px);" transition:slide value={scrollY + innerHeight} max={Math.max(contentHeight, innerHeight)} />
+  {/if}
+</nav>
+
+<style>
+  nav {
+    position: sticky;
+    position: -webkit-sticky;
+    top: 2vh;
+    left: 0;
+    width: 8rem;
+    z-index: 10;
+    align-self: flex-start;
+  }
+
+  nav :global(a:not(.logo)) {
+    text-decoration: none;
+    display: inline-block;
+    padding: 0.33em;
+    margin-bottom: 0.33em;
+    border-radius: 6px;
+  }
+
+  nav :global(a:not(.logo):hover),
+  nav :global(a:not(.logo):focus) {
+    color: black;
+    background-color: white;
+  }
+
+  progress {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    border: none;
+
+    transform: translateX(0.66em) rotate(90deg);
+    transform-origin: top left;
+  }
+
+  progress[value]::-webkit-progress-bar {
+    background: white;
+    border-radius: 2px;
+    height: 2px;
+  }
+
+  progress[value]::progress-bar {
+    background: white;
+    border-radius: 2px;
+    height: 2px;
+  }
+
+  progress[value]::-webkit-progress-value {
+    background: white;
+    border-radius: 6px;
+    height: 4px;
+    position: relative;
+    bottom: 1px;
+  }
+
+  progress[value]::progress-value {
+    background: white;
+    border-radius: 6px;
+    height: 4px;
+    position: relative;
+    bottom: 1px;
+  }
+</style>
--- src/lib/components/Page.svelte
+++ src/lib/components/Page.svelte
@@ -19,25 +19,19 @@
 <script lang="ts">
   import { col, grid } from '$lib/styles/grid.css'
 
-  import { getContext, onMount, setContext } from 'svelte'
-  import { slide } from 'svelte/transition'
-
   import Content from './Content.svelte'
   import Document from './document/Document.svelte'
   import Expander from './Expander.svelte'
   import Footer from './Footer.svelte'
-  import type { Lien } from './Link.svelte'
-  import Link from './Link.svelte'
+  import Navigation from './Navigation.svelte'
   import PageArticles from './PageArticles.svelte'
   import PageIntro from './PageIntro.svelte'
 
 	export let page: Entry<PageDocument>
   export let index = false
-  const { main } = getContext<{ main: Entry<{ links: Entry<Lien>[] }> }>('navigation')
+  
 
-  let scrollY: number
   let offsetHeight: number
-  let innerHeight: number
   let path: string
   // let elements: {[key: string]: Element} = {}
   
@@ -78,37 +72,11 @@
   {/if}
 </svelte:head>
 
-<svelte:window bind:scrollY bind:innerHeight />
+
 
 
 <section class={grid({ columns: 4 })}>
-  <nav>
-    <h5><a href="/">Canada Media Fund</a></h5>
-
-    {#if main}
-    {#each main.fields.links as link}
-    <Link {link} on:click={e => {
-      if (index && !link.fields.path.startsWith('/#')) {
-        e.preventDefault()
-        history.pushState({}, null, link.fields.path)
-        window.scrollTo({ top: document.getElementById(link.fields.path.replace('/', '')).offsetTop, behavior: 'smooth' })
-        path = link.fields.path
-      }
-    }} /><br>
-    {/each}
-    {:else}
-    {#if page.fields.content}
-    {#each page.fields.content.filter(c => c.fields.id) as entry}
-    <a href="#{entry.fields.id}">{entry.fields.title}</a><br>
-    {/each}
-    {/if}
-    {/if}
-
-    {#if scrollY !== undefined}
-    <!-- <progress value={scrollY + 1} max={Math.max(offsetHeight - innerHeight, 1)} /> -->
-    <progress transition:slide value={scrollY + innerHeight} max={Math.max(offsetHeight, innerHeight)} />
-    {/if}
-  </nav>
+  <Navigation contentHeight={offsetHeight} {index} bind:path />
 
   <section bind:offsetHeight class="content {col({ span: 3 })}">
 
@@ -127,67 +95,3 @@
   </section>
 </section>
 
-<style>
-  nav {
-    position: sticky;
-    position: -webkit-sticky;
-    top: 2vh;
-    left: 0;
-    width: 8rem;
-    z-index: 10;
-    align-self: flex-start;
-  }
-
-  nav :global(a:not(.logo)) {
-    text-decoration: none;
-    display: inline-block;
-    padding: 0.33em;
-    margin-bottom: 0.33em;
-    border-radius: 6px;
-  }
-
-  nav :global(a:not(.logo):hover),
-  nav :global(a:not(.logo):focus) {
-    color: black;
-    background-color: white;
-  }
-
-  progress {
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-    border: none;
-
-    transform: translateX(0.66em) rotate(90deg);
-    transform-origin: top left;
-    width: 33vh;
-  }
-
-  progress[value]::-webkit-progress-bar {
-    background: white;
-    border-radius: 2px;
-    height: 2px;
-  }
-
-  progress[value]::progress-bar {
-    background: white;
-    border-radius: 2px;
-    height: 2px;
-  }
-
-  progress[value]::-webkit-progress-value {
-    background: white;
-    border-radius: 6px;
-    height: 4px;
-    position: relative;
-    bottom: 1px;
-  }
-
-  progress[value]::progress-value {
-    background: white;
-    border-radius: 6px;
-    height: 4px;
-    position: relative;
-    bottom: 1px;
-  }
-</style>
