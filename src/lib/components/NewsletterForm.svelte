fa2aa34859ca7c159be54caabb6c66770c8d04bc
Phillip Malboeuf <phil@phils.computer>
2022-01-25 14:31:14 -0500

    newsletter embed

--- src/lib/components/NewsletterForm.svelte
+++ src/lib/components/NewsletterForm.svelte
@@ -12,19 +12,23 @@
   export let entry: Entry<{
     title: string
     id: string
-    url: string
-    emailLabel: string
-    buttonLabel: string
+    embed: string
+    // url: string
+    // emailLabel: string
+    // buttonLabel: string
   }>
   export let color: string = 'muted'
 </script>
 
-<section use:slideIn class="{box({ color: 'outline' })} {grid({ columns: 2 })}" style="--color: {vars.colors[highlights(color)]}">
+<section use:slideIn class="{box({ color: 'outline' })} {grid({ columns: 4 })}" style="--color: {vars.colors[highlights(color)]}">
   {#if entry.fields.title}<h2>{entry.fields.title}</h2>{/if}
-  <form class="{grid({ columns: 2, gap: 'tight' })} {col({ align: 'bottom' })}" action="{entry.fields.url}" target="_blank">
+  <div class="{col({ span: 2 })}">
+    {#if entry.fields.embed}{@html entry.fields.embed}{/if}
+  </div>
+  <!-- <form class="{grid({ columns: 2, gap: 'tight' })} {col({ align: 'bottom' })}" action="{entry.fields.url}" target="_blank">
     <input type="email" class="{inpt()}" placeholder="{entry.fields.emailLabel}">
     <button type="submit" class="{btn({ size: 'tight' })}">{entry.fields.buttonLabel} →</button>
-  </form>
+  </form> -->
 </section>
 
 <style>
@@ -34,6 +38,38 @@
   }
 
   h2 {
+    margin-bottom: 0;
+  }
+
+  div :global(label),
+  div :global(input),
+  div :global(select) {
+    display: block;
+    width: 100%;
+  }
+
+  div :global(input),
+  div :global(select),
+  div :global(button) {
+    font-size: 1.5rem;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+    border: none;
+    color: var(--color);
+    border-bottom: 1px solid currentColor;
+    background: transparent;
+    border-radius: 0;
+    margin-bottom: 1em;
+    margin-top: 0.25em;
+  }
+
+  div :global(button) {
+    font-size: 2rem;
+    text-transform: uppercase;
+    border: 1px solid;
+    border-radius: 6px;
+    padding: 0.5em;
     margin-bottom: 0;
   }
 </style>
