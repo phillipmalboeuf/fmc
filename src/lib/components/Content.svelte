e90c5819a2cadc51b670edeea7d264a99240be91
Phillip Malboeuf <phil@phils.computer>
2022-02-01 16:06:53 -0500

    a bit more space

--- src/lib/components/Content.svelte
+++ src/lib/components/Content.svelte
@@ -61,7 +61,7 @@
   <Picture {media} eager />
 </figure>
 
-<div bind:this={hero} class:hero={!!media && i === 0} id={entry.fields.id} style={`padding-bottom: ${media.fields.file.details.image.height / media.fields.file.details.image.width * 66}%; --back: ${vars.colors[backs(color)]}; --text: ${vars.colors[texts(backs(color))]}`}>
+<div bind:this={hero} class:hero={!!media && i === 0} id={entry.fields.id} style={`padding-bottom: ${media.fields.file.details.image.height / media.fields.file.details.image.width * 88}%; --back: ${vars.colors[backs(color)]}; --text: ${vars.colors[texts(backs(color))]}`}>
 
   <div>
     {#if entry.sys.contentType.sys.id === 'page'}
