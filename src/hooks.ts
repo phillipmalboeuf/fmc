e0c08759dbd37013d8c5600387c1338b35667946
Phillip Malboeuf <phil@phils.computer>
2022-02-08 12:18:30 -0500

    fr fix

--- src/hooks.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve }) {
-
-	const response = await resolve(event)
-	response.headers.set('Link', '</fonts/Trim-Regular.otf>; rel="preload"; as="font"; type="font/otf"; crossorigin="anonymous",</fonts/TrimPoster-Lean.otf>; rel="preload"; as="font"; type="font/otf"; crossorigin="anonymous"')
-	response.headers.set('Cache-Control', 's-maxage=1, stale-while-revalidate')
-
-	return response
-}
