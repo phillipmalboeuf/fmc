c4e8615665262c5df89d2dd19b4a7795d2ebfa2e
Phillip Malboeuf <phil@phils.computer>
2022-02-09 12:56:20 -0500

    google analytics

--- src/routes/[...locale]/__layout.svelte
+++ src/routes/[...locale]/__layout.svelte
@@ -11,6 +11,7 @@
   import type { Entry } from 'contentful'
   import type { Lien } from '$lib/components/Link.svelte'
   import { setContext } from 'svelte'
+  import { page } from '$app/stores'
 
   export let main: Entry<{ links: Entry<Lien>[] }>
   export let secondary: Entry<{ links: Entry<Lien>[] }>
@@ -19,7 +20,36 @@
   setContext('navigation', {
     main, secondary, social, tertiary
   })
+
+  const ga = $page.params.locale === 'fr' ? 'G-2DSGH4HS2M' : 'G-K7F8NWXYL1'
+
+  if (typeof window !== "undefined") {
+		// @ts-ignore
+		window.dataLayer = window.dataLayer || []
+		// @ts-ignore
+		window.gtag = function gtag() {
+			// @ts-ignore
+			window.dataLayer.push(arguments)
+		}
+		// @ts-ignore
+		window.gtag("js", new Date())
+		// @ts-ignore
+		window.gtag("config", ga, { 'send_page_view': false })
+	}
+	$: {
+		// @ts-ignore
+		if (typeof gtag !== "undefined"){
+			// @ts-ignore
+			window.gtag("config", ga, {
+				page_path: $page.url.pathname,
+			})
+		}
+	}
 </script>
+
+<svelte:head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id={ga}"></script>
+</svelte:head>
 
 <main>
   <slot {main} />
