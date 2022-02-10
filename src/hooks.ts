c1911374403e8472d1da538e46da88167a47b02b
Phillip Malboeuf <phil@phils.computer>
2022-02-09 19:43:43 -0500

    fr fix

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,9 +1,14 @@
 /** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve }) {
+export async function handle({ request, resolve }) {
 
-	const response = await resolve(event)
-	response.headers.set('Link', '</fonts/Trim-Regular.otf>; rel="preload"; as="font"; type="font/otf"; crossorigin="anonymous",</fonts/TrimPoster-Lean.otf>; rel="preload"; as="font"; type="font/otf"; crossorigin="anonymous"')
-	response.headers.set('Cache-Control', 's-maxage=1, stale-while-revalidate')
+	const response = await resolve(request)
 
-	return response
+	return {
+		...response,
+		headers: {
+			...response.headers,
+			'Link': '</fonts/Trim-Regular.otf>; rel="preload"; as="font"; type="font/otf"; crossorigin="anonymous",</fonts/TrimPoster-Lean.otf>; rel="preload"; as="font"; type="font/otf"; crossorigin="anonymous"',
+			'Cache-Control': 's-maxage=1, stale-while-revalidate'
+		}
+	}
 }
